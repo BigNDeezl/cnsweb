477dc5a8187c613d9b39c0354a78d9336a2ab82e
CaliNightShift <calinightshift@gmail.com>
2023-10-09 05:52:59 +0000

    Attempting to update the basic python file of the project

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ cnsweb = Flask(__name__)
 
 @cnsweb.route("/")
 def run():
-    return "Hello World"
+    return "Hello Updated Git World"
 
 if __name__ == "__main__":
     cnsweb.run(host="0.0.0.0", port=int("3000"), debug=True)
